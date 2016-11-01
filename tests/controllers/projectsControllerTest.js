79e9ba0c1d88f12fc396f27fd81dc5301735fb91
greenxiii <nikolayenko2009@gmail.com>
2016-11-02 00:33:34 +0200

    create projects

--- tests/controllers/projectsControllerTest.js
+++ tests/controllers/projectsControllerTest.js
@@ -55,87 +55,27 @@ module.exports = function(testData) {
           });
       });
 
-      // it('Should not create projects without title', function (done) {
-      //   request(app)
-      //     .post('/api/users')
-      //     .send({
-      //       'email': 'noywalid.com',
-      //       'password': testData.usersData[1].password,
-      //       'firstName': testData.usersData[1].firstName,
-      //       'lastName': testData.usersData[1].lastName,
-      //     })
-      //     .end(function(err,res) {
-      //       res.status.should.be.equal(400);
-      //       res.body.message.should.be.a('Object');
-      //       res.body.message.email[0].should.be.equal('Should be valid email address');
-      //       done();
-      //     });
-      // });
+      it('Should not create project without title', function (done) {
+        var user1;
+        request(app)
+          .post('/api/users')
+          .send(testData.usersData[0])
+          .then(function(res) {
+            res.status.should.be.equal(200);
+            res.body.token.should.be.a('string');
+            user1 = res.body.user;
+            user1.token = res.body.token;
 
-      // it('Should not create user without email', function (done) {
-      //   request(app)
-      //     .post('/api/users')
-      //     .send({
-      //       'password': testData.usersData[1].password,
-      //       'firstName': testData.usersData[1].firstName,
-      //       'lastName': testData.usersData[1].lastName,
-      //     })
-      //     .end(function(err,res) {
-      //       res.status.should.be.equal(400);
-      //       res.body.message.should.be.a('Object');
-      //       res.body.message.email[0].should.be.equal('Can\'t be blank');
-      //       res.body.message.email[1].should.be.equal('Should be valid email address');
-      //       done();
-      //     });
-      // });
-
-      // it('Should not create user without firstName', function (done) {
-      //   request(app)
-      //     .post('/api/users')
-      //     .send({
-      //       'email': testData.usersData[1].email,
-      //       'password': testData.usersData[1].password,
-      //       'lastName': testData.usersData[1].lastName
-      //     })
-      //     .end(function(err,res) {
-      //       res.status.should.be.equal(400);
-      //       res.body.message.should.be.a('Object');
-      //       res.body.message.firstName[0].should.be.equal('Can\'t be blank');
-      //       done();
-      //     });
-      // });
-
-      // it('Should not create user without lastName', function (done) {
-      //   request(app)
-      //     .post('/api/users')
-      //     .send({
-      //       'email': testData.usersData[1].email,
-      //       'password': testData.usersData[1].password,
-      //       'firstName': testData.usersData[1].firstName
-      //     })
-      //     .end(function(err,res) {
-      //       res.status.should.be.equal(400);
-      //       res.body.message.should.be.a('Object');
-      //       res.body.message.lastName[0].should.be.equal('Can\'t be blank');
-      //       done();
-      //     });
-      // });
-
-      // it('Should not create user without password', function (done) {
-      //   request(app)
-      //     .post('/api/users')
-      //     .send({
-      //       'email': testData.usersData[1].email,
-      //       'firstName': testData.usersData[1].firstName,
-      //       'lastName': testData.usersData[1].lastName
-      //     })
-      //     .end(function(err,res) {
-      //       res.status.should.be.equal(400);
-      //       res.body.message.should.be.a('Object');
-      //       res.body.message.password[0].should.be.equal('Can\'t be blank');
-      //       done();
-      //     });
-      // });
+            return request(app)
+              .post('/api/projects')
+              .set('Authorization', 'Bearer ' + user1.token)
+          })
+          .then(function(res) {
+            res.status.should.be.equal(400);
+            res.body.message.title[0].should.equal('Can\'t be blank');
+            done();
+          });
+      });
     });
   });
 };
